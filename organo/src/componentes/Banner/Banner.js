5994e0866c56db172b272859fb5332749505b041
Vinícius Mosna D'Orazio <viniw.md@gmail.com>
2022-10-13 02:04:59 -0300

    Aula 2

---
+++ organo/src/componentes/Banner/Banner.js
@@ -0,0 +1,10 @@
+import'./Banner.css';
+
+function Banner() {
+    return(
+        <header className='banner'>
+            <img src="/imagens/banner.png" alt="em un fundo azul está escrito: organo! Pessoas e times organizados em um só lugar! e há algumas formas geométricas e a foto de várias pessoas com uma das mãos colocadas uma sobre as outras no centro "/>
+        </header>
+    )
+}
+export default Banner
