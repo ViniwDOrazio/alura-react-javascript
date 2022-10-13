05f3dd59734a345bad5742ce25c53a2c58715d2b
Vinícius Mosna D'Orazio <viniw.md@gmail.com>
2022-10-13 02:20:34 -0300

    Aula 3

--- organo/src/App.js
+++ organo/src/App.js
@@ -1,28 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
 import Banner from './componentes/Banner/Banner';
 
 function App() {
   return (
     <div className="App">
       <Banner />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Bem vindo ao React! Esse é o famigerado hello Word da linguagem.
-        </p>
-        <p>
-          Edite <code>src/App.js</code> e salve para recarregar.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
