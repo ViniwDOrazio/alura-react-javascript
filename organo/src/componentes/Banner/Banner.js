e534cd22ff0895b18dced5646877f28bec0e62dc
Vinícius Mosna D'Orazio <viniw.md@gmail.com>
2022-10-13 23:50:24 -0300

    aula 2 e 3, personalização do tema

--- organo/src/componentes/Banner/Banner.js
+++ organo/src/componentes/Banner/Banner.js
@@ -3,7 +3,7 @@ import'./Banner.css';
 function Banner() {
     return(
         <header className='banner'>
-            <img src="/imagens/banner.png" alt="em un fundo azul está escrito: organo! Pessoas e times organizados em um só lugar! e há algumas formas geométricas e a foto de várias pessoas com uma das mãos colocadas uma sobre as outras no centro "/>
+            <img src="/imagens/banner.png" alt="organo! a linhagrm do dragão, seguido do simbolo da casa targaryen"/>
         </header>
     )
 }
