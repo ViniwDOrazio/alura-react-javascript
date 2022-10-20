63e1d38f1bee6af01fa442893e1dd1d217607723
Vinícius Mosna D'Orazio <viniw.md@gmail.com>
2022-10-20 00:33:49 -0300

    qualidade de vida - populado

--- organo/src/componentes/Conteudo/Familia/index.js
+++ organo/src/componentes/Conteudo/Familia/index.js
@@ -2,18 +2,23 @@ import Pessoa from '../Pessoa';
 import './Familia.css'
 
 const Familia = (props) => {
-
-
     return (
+        (props.pessoas.length > 0) && 
+        //(props.pessoas.length > 0) ? 
         <section className='familia' style={{backgroundColor: props.corBackground}}>
             <h3 style={{borderColor: props.corMarcacao}}>
                 {props.nome}
             </h3>
             <div className='pessoas'>
-                {props.pessoas.map(pessoa => <Pessoa nome={pessoa.nome} cargo={pessoa.cargo} imagem={pessoa.imagem}/>)}
+                {props.pessoas.map(pessoa => <Pessoa nome={pessoa.nome} 
+                                                     key={pessoa.nome} 
+                                                     cargo={pessoa.cargo} 
+                                                     imagem={pessoa.imagem}
+                                                     cor={props.corDestaque}
+                                                     corFonte={props.corMarcacao}/>)}
             </div>
         </section>
-
+        //: ""
     )
 }
 
--- organo/src/componentes/Conteudo/Pessoa/index.js
+++ organo/src/componentes/Conteudo/Pessoa/index.js
@@ -1,13 +1,13 @@
 import './Pessoa.css'
 
-const Pessoa = ({nome, imagem, cargo}) => {
+const Pessoa = ({nome, imagem, cargo, cor, corFonte}) => {
     return (
         <div className='pessoa'>
-            <div className='cabecalho'>
+            <div className='cabecalho' style={{backgroundColor: cor}}>
                 <img src={imagem} alt={nome} />
             </div>
             <div className='rodape'>
-                <h4>{nome}</h4>
+                <h4 style={{color: corFonte}}>{nome}</h4>
                 <h5>{cargo}</h5>
             </div>
         </div>
