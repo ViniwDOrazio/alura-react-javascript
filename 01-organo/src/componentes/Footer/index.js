61e9108f2d42940bb2a43efcc0524e0111b74c6c
Vinícius Mosna D'Orazio <viniw.md@gmail.com>
2022-12-07 01:11:16 -0300

    Resolvido conflito

--- 01-organo/src/componentes/Footer/index.js
+++ 01-organo/src/componentes/Footer/index.js
@@ -4,9 +4,9 @@ function Footer({facebook, instagram, twitter}) {
     return(
         <footer className='footer'>
             <section className='social coluna1de3'>
-                <a href={facebook} target="_blank"><img src="/imagens/footer/facebook.png" alt="facebook"/></a>
-                <a href={instagram} target="_blank"><img src="/imagens/footer/instagram.png" alt="instagram"/></a>
-                <a href={twitter} target="_blank"><img src="/imagens/footer/twitter.png" alt="twitter"/></a>
+                <a href={facebook} target="_blank" rel="noreferrer"><img src="/imagens/footer/facebook.png" alt="facebook"/></a>
+                <a href={instagram} target="_blank" rel="noreferrer"><img src="/imagens/footer/instagram.png" alt="instagram"/></a>
+                <a href={twitter} target="_blank"rel="noreferrer"><img src="/imagens/footer/twitter.png" alt="twitter"/></a>
             </section>
             <section className='logo coluna1de3'>
                 <img src="/imagens/footer/logo.png" alt="twitter"/>
