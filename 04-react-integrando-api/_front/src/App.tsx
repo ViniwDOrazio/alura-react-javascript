1b9b91a6d9222fea3e45be8fc9258b834eee3414
Vinícius Mosna D'Orazio <viniw.md@gmail.com>
2023-08-01 02:13:26 -0300

    Curso 05 - Aula 06

--- 04-react-integrando-api/_front/src/App.tsx
+++ 04-react-integrando-api/_front/src/App.tsx
@@ -4,6 +4,8 @@ import VitrineRestaurantes from './paginas/VitrineRestaurantes';
 import AdminisracaoRestaurantes from './paginas/Administracao/Restautantes';
 import RestaurantesFormulario from './paginas/Administracao/Restautantes/Formulario';
 import PaginaBaseAdmin from './paginas/Administracao';
+import AdminisracaoPratos from './paginas/Administracao/Pratos';
+import PratosFormulario from './paginas/Administracao/Pratos/Formulario';
 
 function App() {
 
@@ -15,6 +17,10 @@ function App() {
         <Route path="restaurantes" element={<AdminisracaoRestaurantes />} />
         <Route path="restaurantes/novo" element={<RestaurantesFormulario />} />
         <Route path="restaurantes/:id" element={<RestaurantesFormulario />} />
+
+        <Route path="pratos" element={<AdminisracaoPratos />} />
+        <Route path="pratos/novo" element={<PratosFormulario />} />
+        <Route path="pratos/:id" element={<PratosFormulario />} />
       </Route>
 
     </Routes>
